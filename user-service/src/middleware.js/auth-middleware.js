ccd5d87d78a9e65e36cebf41975761aa97ccb856
Govind Singh <govindkmr141@gmail.com>
2024-10-07 13:37:31 +0530

    added middleware

--- user-service/src/middleware.js/auth-middleware.js
+++ user-service/src/middleware.js/auth-middleware.js
@@ -1,30 +1,36 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const { FORBIDDEN_ERROR } = require('../constants/error-constants');
-const { STATUS_FORBIDDEN } = require('../constants/status-codes');
+const {
+  FORBIDDEN_ERROR,
+  UNAUTHORIZED_ERROR,
+} = require("../constants/error-constants");
+const {
+  STATUS_FORBIDDEN,
+  STATUS_UNAUTHORIZED,
+} = require("../constants/status-codes");
 
-const secretKey = process.env.JWT_SECRET || 'SOME_VERY_STRONG_SECRET_KEY';
+const secretKey = process.env.JWT_SECRET || "SOME_VERY_STRONG_SECRET_KEY";
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
-    
-    if (!token) {
-        const error = new Error("User is not authorized!");
-        error.name = FORBIDDEN_ERROR;
-        error.status = STATUS_FORBIDDEN;
-        throw error;
-    }
+  const token = req.headers["authorization"]?.split(" ")[1];
 
-    jwt.verify(token, secretKey, (err, decoded) => {
-        if (err) {
-            const error = new Error("User is not authorized!");
-            error.name = FORBIDDEN_ERROR;
-            error.status = STATUS_FORBIDDEN;
-            throw error;
-        }
-        req.user = decoded; 
-        next();
-    });
+  if (!token) {
+    const error = new Error("User is not authorized!");
+    error.name = UNAUTHORIZED_ERROR;
+    error.status = STATUS_UNAUTHORIZED;
+    throw error;
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      const error = new Error("User is not authorized!");
+      error.name = UNAUTHORIZED_ERROR;
+      error.status = STATUS_UNAUTHORIZED;
+      throw error;
+    }
+    req.user = decoded;
+    next();
+  });
 };
 
 module.exports = verifyToken;
